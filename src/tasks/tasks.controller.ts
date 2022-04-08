7f5a97a5b706ed5888e9cda0ee397c42cb2600c8
Artem(Boss of the Gym) <aachulaevskiy@miem.hse.ru>
2022-04-08 22:27:51 +0300

    documentation_update

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { TaskTemplateInterface } from "./dto/task-template.interface";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
-import { StudyMaterial } from "../study-materials/study-materials.model";
 import { Task } from "./tasks.model";
+import {GeneratedTaskDto} from "./dto/generated-task-dto";
 
 @ApiTags('Tasks')
 @Controller('tasks')
@@ -40,26 +39,33 @@ export class TasksController {
     return this.tasksService.getTemplateByLangAndSection(lang, section)
   }
 
+  @ApiOperation({summary: 'Получение сгенерированного задания по id'})
+  @ApiResponse({status: 201, type: GeneratedTaskDto})
   @Get('generate/getById/:id')
   getGeneratedById(@Param('id') id: string) {
     return this.tasksService.getGeneratedById(Number(id))
   }
 
+  @ApiOperation({summary: 'Получение сгенерированных заданий по языку и разделу'})
+  @ApiResponse({status: 201, type: [CreateTaskDto]})
   @Get('generate/getByParams/:lang/:section')
   getGeneratedByLangAndSection(@Param('lang') lang: string, @Param('section') section: string) {
     return this.tasksService.getGeneratedByLangAndSection(lang, section)
   }
 
+  @ApiOperation({summary: 'Обновление шаблона задания'})
   @Put('template/:id')
   updateTemplateById(@Param('id') id: string, dto: CreateTaskDto) {
     return this.tasksService.updateTemplate(dto, Number(id))
   }
 
+  @ApiOperation({summary: 'Удаление шаблона задания'})
   @Delete('template/:id')
   deleteTemplateById(@Param('id') id: string) {
     return this.tasksService.deleteTemplate(Number(id))
   }
 
+  @ApiOperation({summary: 'Проверка генерации задания из шаблона'})
   @Post('test')
   test(@Body() dto: CreateTaskDto) {
     return this.tasksService.test(dto)
